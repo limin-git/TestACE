06a5abebe82cda4022eec86a36ba6f27e05edb0d
zhulimin <zhulimin.china@gmail.com>
2016-08-04 23:03:56 +0800

    error handle

--- capture_ace.d
+++ capture_ace.d
@@ -4,7 +4,6 @@
 BEGIN
 {
 	system( "./AceLock mylog myace new &" );
-	ace_lock_exited = 1;
 }
 
 
@@ -15,27 +14,11 @@ proc:::exec-success
 	acelock_pid = curpsinfo->pr_pid;
 	printf( "%s %d \n", execname, acelock_pid);
 	system( "./ace_lock.d %d", acelock_pid );
-	ace_lock_exited = 0;
+	exit(0);
 }
 
 
-proc:::exit
-/execname == "AceLock"/
+END
 {
-	ace_lock_exited = 1;
-}
-
-
-proc:::exit
-/execname == "ace_lock.d" && ace_lock_exited == 0/
-{
-	trace( "kill AceLock\n" );
 	system( "prun %d", acelock_pid );
-}
-
-
-proc:::exit
-/execname == "ace_lock.d"/
-{
-	exit( 0 );
 }
